16c439e0c15758b67fc8ec323d45575a4ad31508
Bidisha <dishabose96@gmail.com>
2021-02-18 14:33:34 +0530

    debugging

---
+++ src/Counter.java
@@ -0,0 +1,16 @@
+
+public class Counter {
+	int result=0;
+
+	public int getResult() {
+		System.out.println("returns value");
+		return result;
+	}
+
+	public void setResult(int result) {
+		System.out.println("set");
+		this.result = result;
+	}
+	
+
+}
---
+++ src/CounterDemo.java
@@ -0,0 +1,13 @@
+
+public class CounterDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Counter c = new Counter();
+		c.setResult(10);
+		c.getResult();
+		
+
+	}
+
+}
